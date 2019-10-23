08b96600913e415953d3af34c634345e5576534b
Kevin Harvell <kevharvell@gmail.com>
2019-10-22 21:50:06 -0500

    Added a button to call API and log result data to console; data is stored as a state variable

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    APIData: [],
+    fromYear: 2018
+  }
+
+  handleAPIClick = () => {
+    fetch('https://api.census.gov/data/timeseries/eits/resconst?get=cell_value,data_type_code,time_slot_id,error_data,category_code,seasonally_adj&time=from+' + this.state.fromYear)
+        .then(res => res.json())
+        .then((data) => {
+          this.setState({
+            APIData: data
+          });
+          console.log(this.state.APIData);
+        })
+        .catch(console.log)
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.handleAPIClick}>Click to test API</button>
+        <div>
+          <h2>Data in Console</h2>
+          <p>Data is stored in the state as APIData</p>
+        </div>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
