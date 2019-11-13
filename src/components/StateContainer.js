4b99162fd782aeff4b99785ffbe261dd32b56912
Drot88 <Daniel.Rotenberg88@gmail.com>
2019-11-13 13:19:14 -0600

    Adding in Accordion

--- src/components/StateContainer.js
+++ src/components/StateContainer.js
@@ -3,6 +3,15 @@ import GMap from './GMap';
 import DataForm from './DataForm';
 import Chart from './Chart';
 import Spinner from './Spinner'
+import '../styles/Accordion.css'
+ 
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemHeading,
+    AccordionItemPanel,
+    AccordionItemButton
+} from 'react-accessible-accordion';
 
 export class StateContainer extends Component {
 
@@ -106,56 +115,86 @@ export class StateContainer extends Component {
   render() {
     return (
       <div>
-        <GMap
-          latlng={this.state.latlng}
-          address={this.state.address}
-          stateLong={this.state.stateLong}
-          stateShort={this.state.stateShort}
-          postalCode={this.state.postalCode}
-          region={this.state.region}
-          updateMapState={this.updateMapState}
-        />
-        <DataForm
-          tableName={this.state.tableName}
-          fromYear={this.state.fromYear}
-          graphData={this.state.graphData}
-          timeStart={this.state.timeStart}
-          timeEnd={this.state.timeEnd}
-          dataType={this.state.dataType}
-          category={this.state.category}
-          labels={this.state.labels}
-          chartData={this.state.chartData}
-          tableData={this.state.tableData}
-          dropDown={this.state.dropDown}
-          dataTypes={this.state.dataTypes}
-          categories={this.state.categories}
-          tableId={this.state.tableId}
-          region={this.state.region}
-          stateShort={this.state.stateShort}
-          location={this.state.location}
-          updateFormState={this.updateFormState}
-          updateCategoryState={this.updateCategoryState}
-          updateDataTypeState={this.updateDataTypeState}
-          updateGraphState={this.updateGraphState}
-          updateTableState={this.updateTableState}
-          updateTimeState={this.updateTimeState}
-          updateLoading={this.updateLoading}
-        />
-        <div>
-          {
-            this.state.loading &&
-            !this.state.renderChart &&
-            <Spinner />
-          }
-        </div>
-        {
-          this.state.renderChart &&
-          <Chart
-            labels={this.state.labels}
-            data={this.state.chartData}
-            title={this.state.tableName}
-          />
-        }
+        <Accordion>
+          <AccordionItem>
+            <AccordionItemHeading>
+                <AccordionItemButton>
+                    Map
+                </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel>
+              <GMap
+                latlng={this.state.latlng}
+                address={this.state.address}
+                stateLong={this.state.stateLong}
+                stateShort={this.state.stateShort}
+                postalCode={this.state.postalCode}
+                region={this.state.region}
+                updateMapState={this.updateMapState}
+              />
+            </AccordionItemPanel>
+          </AccordionItem>
+
+          <AccordionItem>
+            <AccordionItemHeading>
+                <AccordionItemButton>
+                    Form
+                </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel>
+              <DataForm
+                tableName={this.state.tableName}
+                fromYear={this.state.fromYear}
+                graphData={this.state.graphData}
+                timeStart={this.state.timeStart}
+                timeEnd={this.state.timeEnd}
+                dataType={this.state.dataType}
+                category={this.state.category}
+                labels={this.state.labels}
+                chartData={this.state.chartData}
+                tableData={this.state.tableData}
+                dropDown={this.state.dropDown}
+                dataTypes={this.state.dataTypes}
+                categories={this.state.categories}
+                tableId={this.state.tableId}
+                region={this.state.region}
+                stateShort={this.state.stateShort}
+                location={this.state.location}
+                updateFormState={this.updateFormState}
+                updateCategoryState={this.updateCategoryState}
+                updateDataTypeState={this.updateDataTypeState}
+                updateGraphState={this.updateGraphState}
+                updateTableState={this.updateTableState}
+                updateTimeState={this.updateTimeState}
+                updateLoading={this.updateLoading}
+              />
+            </AccordionItemPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <AccordionItemHeading>
+                <AccordionItemButton>
+                    Chart
+                </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel>
+              <div>
+              {
+                this.state.loading &&
+                !this.state.renderChart &&
+                <Spinner />
+              }
+              </div>
+              { 
+                this.state.renderChart &&
+                <Chart
+                  labels={this.state.labels}
+                  data={this.state.chartData}
+                  title={this.state.tableName}
+                />
+              }
+            </AccordionItemPanel>
+          </AccordionItem>
+        </Accordion>
       </div>
     );
   }
