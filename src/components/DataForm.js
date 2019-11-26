522fdcb30574bf5ca79f8a33b2646e5d5badb4d8
kevharvell <kevharvell@gmail.com>
2019-11-25 21:40:57 -0600

    Fixed a lot of the API errors and added an alert if there is not data to show (#16)

--- src/components/DataForm.js
+++ src/components/DataForm.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import Select from 'react-select';
-import "../styles/MapForm.css"
+import React, { Component } from "react";
+import Select from "react-select";
+import "../styles/MapForm.css";
 
 const CATEGORY_CODE = 0;
 const DATA_TYPE_CODE = 1;
@@ -8,17 +8,16 @@ const GEO_LEVEL_CODE = 2;
 
 const style = {
   form: {
-    margin: '20px'
+    margin: "20px"
   },
   select: {
     control: () => ({
       width: 50
     })
   }
-}
+};
 
 class DataForm extends Component {
-
   constructor(props) {
     super(props);
     this.handleTableName = this.handleTableName.bind(this);
@@ -28,63 +27,90 @@ class DataForm extends Component {
     await this.getAPIData();
   }
 
-  handleDataType = (selectedOption) => {
+  handleDataType = selectedOption => {
     const dataType = selectedOption.value;
     this.props.updateDataTypeState(dataType);
-  }
+  };
 
-  handleCategory = (selectedOption) => {
+  handleCategory = selectedOption => {
     const category = selectedOption.value;
     this.props.updateCategoryState(category);
-  }
+  };
 
   async handleTableName(selectedOption) {
-    const tableId = selectedOption.value
-    const tableName = selectedOption.label
-    let location = "US"
-    const data = await import(`../apiJsons/${tableId}.json`)
+    const tableId = selectedOption.value;
+    const tableName = selectedOption.label;
+    let location = "US";
+    const data = await import(`../apiJsons/${tableId}.json`);
 
-    let dataTypes = this.extractCategoryDataDropdownOptions(data.default[tableId][DATA_TYPE_CODE].data_type_code);
-    let categories = this.extractCategoryDataDropdownOptions(data.default[tableId][CATEGORY_CODE].category_code);
-    let geoLevelCodes = this.extractCategoryDataDropdownOptions(data.default[tableId][GEO_LEVEL_CODE].geo_level_code);
+    let dataTypes = this.extractCategoryDataDropdownOptions(
+      data.default[tableId][DATA_TYPE_CODE].data_type_code
+    );
+    let categories = this.extractCategoryDataDropdownOptions(
+      data.default[tableId][CATEGORY_CODE].category_code
+    );
+    let geoLevelCodes = this.extractCategoryDataDropdownOptions(
+      data.default[tableId][GEO_LEVEL_CODE].geo_level_code
+    );
     if (geoLevelCodes.length === 5) {
       location = this.props.region;
     }
     // else if (geoLevelCodes.length > 5) {
     //   location = this.props.stateShort;
     // }
-    this.props.updateTableState(tableName, dataTypes, categories, tableId, this.props.tableData, this.props.dropDown, location);
+    this.props.updateTableState(
+      tableName,
+      dataTypes,
+      categories,
+      tableId,
+      this.props.tableData,
+      this.props.dropDown,
+      location
+    );
   }
 
-  handleChange = (event) => {
-    const timeStart = event.target.value
-    const fromYear = timeStart.split('-')[0]
-    this.props.updateTimeState(timeStart, this.props.timeEnd, fromYear)
-  }
+  handleChange = event => {
+    const timeStart = event.target.value;
+    const fromYear = timeStart.split("-")[0];
+    this.props.updateTimeState(timeStart, this.props.timeEnd, fromYear);
+  };
 
-  getAPIGraphData = (event) => {
+  getAPIGraphData = event => {
     this.props.updateLoading(true);
-    event.preventDefault()
-    fetch(`https://api.census.gov/data/timeseries/eits/${this.props.tableId}?get=cell_value,data_type_code,time_slot_id,error_data,category_code,seasonally_adj,geo_level_code&time=from+${this.props.fromYear}`)
+    event.preventDefault();
+    let apiUrl;
+    if (
+      this.props.tableId === "advm3" ||
+      this.props.tableId === "bfs" ||
+      this.props.tableId === "m3" ||
+      this.props.tableId === "mhs2"
+    ) {
+      apiUrl = `https://api.census.gov/data/timeseries/eits/${this.props.tableId}?get=cell_value,data_type_code,time_slot_id,error_data,category_code,seasonally_adj,geo_level_code&time=from+${this.props.fromYear}&for=US`;
+    } else {
+      apiUrl = `https://api.census.gov/data/timeseries/eits/${this.props.tableId}?get=cell_value,data_type_code,time_slot_id,error_data,category_code,seasonally_adj,geo_level_code&time=from+${this.props.fromYear}`;
+    }
+    fetch(apiUrl)
       .then(res => res.json())
       .then(data => {
-        console.log(data)
+        console.log(data);
         this.parseApiResponse(data);
       })
-      .catch(console.log)
-  }
+      .catch(console.log);
+  };
 
-  getTables = (apiData) => {
-    let tableData = []
+  getTables = apiData => {
+    let tableData = [];
 
     apiData.dataset.forEach(table => {
-      const newObj = {
-        "id": table.c_dataset[2], // id needed for graph data API call URL
-        "title": table.title.split(': ')[1], // takes table title description after ': '
-        "varLink": table.c_variablesLink,
-        "fromYear": table.temporal.match(/[1|2]\d+/g).map(Number)[0] // parses 1000/2000 years from string ex. 1968, 2005
+      if (table.c_dataset[2] !== "hv" && table.c_dataset[2] !== "mhs") {
+        const newObj = {
+          id: table.c_dataset[2], // id needed for graph data API call URL
+          title: table.title.split(": ")[1], // takes table title description after ': '
+          varLink: table.c_variablesLink,
+          fromYear: table.temporal.match(/[1|2]\d+/g).map(Number)[0] // parses 1000/2000 years from string ex. 1968, 2005
+        };
+        tableData.push(newObj);
       }
-      tableData.push(newObj);
     });
 
     let dropDown = this.extractTableDropdownOptions(tableData);
@@ -96,79 +122,97 @@ class DataForm extends Component {
       this.props.tableId,
       tableData,
       dropDown
-    )
-  }
+    );
+  };
 
-  extractTableDropdownOptions = (tableObjects) => {
-    let selectOptions = []
+  extractTableDropdownOptions = tableObjects => {
+    let selectOptions = [];
     tableObjects.forEach(table => {
-      selectOptions.push(
-        { value: table.id, label: table.title }
-      )
-    })
-    return selectOptions
-  }
+      selectOptions.push({ value: table.id, label: table.title });
+    });
+    return selectOptions;
+  };
 
-  extractCategoryDataDropdownOptions = (objects) => {
-    let selectOptions = []
+  extractCategoryDataDropdownOptions = objects => {
+    let selectOptions = [];
     objects.forEach(obj => {
-      selectOptions.push(
-        { value: obj.id, label: obj.description }
-      )
-    })
-    return selectOptions
-  }
+      selectOptions.push({ value: obj.id, label: obj.description });
+    });
+    return selectOptions;
+  };
 
   async getAPIData() {
-    fetch('https://api.census.gov/data/timeseries/eits')
+    fetch("https://api.census.gov/data/timeseries/eits")
       .then(res => res.json())
       .then(data => {
-        this.getTables(data)
+        this.getTables(data);
       })
-      .catch(console.log)
+      .catch(console.log);
   }
 
-  parseApiResponse = (data) => {
-    let labels = []
-    let chartData = []
+  invalidComboAlert = () => {
+    alert(
+      "There is no data for the combination you selected. Please try another combination."
+    );
+  };
+
+  parseApiResponse = data => {
+    let labels = [];
+    let chartData = [];
     data.forEach(row => {
-      if (row[1] === this.props.dataType && row[4] === this.props.category && row[5] === "no" && row[6] === this.props.location) {
+      if (
+        row[1] === this.props.dataType &&
+        row[4] === this.props.category &&
+        row[5] === "no" &&
+        row[6] === this.props.location
+      ) {
         chartData.push(row[0]);
-        labels.push(row[7])
+        labels.push(row[7]);
       }
-    })
+    });
+    console.log(chartData);
+    console.log(labels);
+    if (chartData.length === 0 || labels.length === 0) {
+      this.invalidComboAlert();
+      return;
+    }
     this.props.updateGraphState(this.props.graphData, labels, chartData, true);
     this.props.updateLoading(false);
-  }
+  };
 
   render() {
     return (
       <div>
         <div className="mapForm">
-          <h1>Please Select a Report<span>Make sure to fill out the time period you would like to see.</span></h1>
+          <h1>
+            Please Select a Report
+            <span>
+              Make sure to fill out the time period you would like to see.
+            </span>
+          </h1>
           <form onSubmit={this.getAPIGraphData} style={style.form}>
             <Select
-              placeholder='Select Table'
+              placeholder="Select Table"
               style={style.select}
               onChange={this.handleTableName}
               options={this.props.dropDown}
             />
             <Select
-              placeholder='Select Data Type'
+              placeholder="Select Data Type"
               style={style.select}
               onChange={this.handleDataType}
               options={this.props.dataTypes}
             />
             <Select
-              placeholder='Select Category'
+              placeholder="Select Category"
               style={style.select}
               onChange={this.handleCategory}
               options={this.props.categories}
             />
             <div className="inner-wrap">
-              <label htmlFor='timeStart'>Time Period Start</label>
+              <label htmlFor="timeStart">Time Period Start</label>
               <input
-                name='timeStart'
+                name="timeStart"
                 placeholder="Time Period Start"
                 type="date"
                 value={this.props.timeStart}
@@ -186,4 +230,4 @@ class DataForm extends Component {
   }
 }
 
-export default DataForm
+export default DataForm;
